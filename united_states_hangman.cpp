07b0c7accc6aa7afdbb20c8d77bb37b881393d49
justinmeredith <jm.4444@icloud.com>
2026-06-23 14:48:47 -0500

    Updates all arrays to be vectors to allow for cleaner parameter passes. Adds a vector containing the US state capitals to be used in the future for difficulty settings.

--- united_states_hangman.cpp
+++ united_states_hangman.cpp
@@ -12,7 +12,9 @@
 using namespace std;
 
 // Data
-const string usa_states[50] = {"Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"};
+const vector<string> usa_states = {"Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming", "Washington DC"};
+
+const vector<string> usa_capitals = {"Montgomery", "Juneau", "Phoenix", "Little Rock", "Sacramento", "Denver", "Hartford", "Dover", "Tallahassee", "Atlanta", "Honolulu", "Boise", "Springfield", "Indianapolis", "Des Moines", "Topeka", "Frankfort", "Baton Rouge", "Augusta", "Annapolis", "Boston", "Lansing", "Saint Paul", "Jackson", "Jefferson City", "Helena", "Lincoln", "Carson City", "Concord", "Trenton", "Santa Fe", "Albany", "Raleigh", "Bismarck", "Columbus", "Oklahoma City", "Salem", "Harrisburg", "Providence", "Columbia", "Pierre", "Nashville", "Austin", "Salt Lake City", "Montpelier", "Richmond", "Olympia", "Charleston", "Madison", "Cheyenne"};
 
 vector<string> hangman = {
     "  __\n |  |\n |\n |\n |\n_|_\n",
@@ -27,10 +29,10 @@ vector<string> hangman = {
 // Functions
 
 // Returns a random state
-string selectRandomState(const string usa_states[50]) {
+string selectRandomState(const vector<string>& usa_array) {
     static mt19937 random_number_generator(random_device{}());
-    uniform_int_distribution<int> range(0, 49);
-    return usa_states[range(random_number_generator)];
+    uniform_int_distribution<int> range(0, usa_array.size() - 1);
+    return usa_array.at(range(random_number_generator));
 };
 
 // Returns a vector of underscores equivalent to the number of letters in the selected state
